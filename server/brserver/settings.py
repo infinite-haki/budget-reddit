7a6d861da63fdc13d2677ab5c78beeb7fb7bda1e
infinite-haki <leonardthang2020@gmail.com>
2023-03-27 02:13:28 -0500

    Added postgres to database setting

--- server/brserver/settings.py
+++ server/brserver/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'brserver.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'budget_reddit',
+        'USER': config("DB_USER"),
+        'PASSWORD': config("DB_USER_PW"),
+        'HOST': config("DB_HOST"),
+        'PORT': config("DB_PORT"),
     }
 }
 
